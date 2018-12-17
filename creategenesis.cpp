1fcbc204cec4c8aad7a2205aa7dd389d2761252a
Nikko <40539492+NicolasChoukroun@users.noreply.github.com>
2018-12-17 21:29:54 +0700

    Update creategenesis.cpp

--- creategenesis.cpp
+++ creategenesis.cpp
@@ -5,7 +5,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-Mining algorithm
+//Mining algorithm
 
 const arith_uint256 maxUint = UintToArith256(
         uint256S("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
